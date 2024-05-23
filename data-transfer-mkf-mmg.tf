81134a37951173f92e146f8c752c475806b0c6a9
Nadezhda-Subotka <144116230+Nadezhda-Subotka@users.noreply.github.com>
2024-05-23 14:38:28 +0300

    YCDOCIO-3558: Updated Terraform manifest (#1)
    * YCDOCIO-3558: Updated Terraform manifest
    
    * YCDOCIO-3558: Grammatical edits

--- data-transfer-mkf-mmg.tf
+++ data-transfer-mkf-mmg.tf
@@ -1,33 +1,47 @@
-# Infrastructure for the Yandex Cloud Managed Service for Apache Kafka®, Managed Service for MongoDB and Data Transfer
+# Infrastructure for the Yandex Cloud Managed Service for Apache Kafka®, Managed Service for MongoDB, and Data Transfer
 #
-# RU: https://cloud.yandex.ru/docs/data-transfer/tutorials/data-transfer-mkf-mmg
-# EN: https://cloud.yandex.com/en/docs/data-transfer/tutorials/data-transfer-mkf-mmg
+# RU: https://yandex.cloud/ru/docs/data-transfer/tutorials/mkf-to-mmg
+# EN: https://yandex.cloud/en/docs/data-transfer/tutorials/mkf-to-mmg
+
 #
 # Specify the following settings:
 locals {
   # Source Managed Service for Apache Kafka® cluster settings:
-  source_kf_version    = "" # Set a desired version of Apache Kafka®. For available versions, see the documentation main page: https://cloud.yandex.com/en/docs/managed-kafka/
-  source_user_password = "" # Set a password for the Apache Kafka® user.
+  source_kf_version    = "" # Desired version of Apache Kafka®. For available versions, see the documentation main page: https://yandex.cloud/en/docs/managed-kafka/.
+  source_user_password = "" # Apache Kafka® user's password
 
   # Target Managed Service for MongoDB cluster settings:
-  target_mg_version    = "" # Set a desired version of MongoDB. For available versions, see the documentation main page: https://cloud.yandex.com/en/docs/managed-mongodb/
-  target_user_password = "" # Set a password for the MongoDB user.
+  target_mg_version    = "" # Desired version of MongoDB. For available versions, see the documentation main page: https://yandex.cloud/en/docs/managed-mongodb/.
+  target_user_password = "" # MongoDB user's password
 
   # Specify these settings ONLY AFTER the clusters are created. Then run "terraform apply" command again.
-  # You should set up endpoints using the GUI to obtain their IDs.
-  source_endpoint_id = "" # Set the source endpoint ID.
-  target_endpoint_id = "" # Set the target endpoint ID.
-  transfer_enabled   = 0  # Set to 1 to enable Transfer.
+  # You should set up endpoints using the GUI to obtain their IDs
+  source_endpoint_id = "" # Source endpoint ID
+  target_endpoint_id = "" # Target endpoint ID
+  transfer_enabled   = 0  # Set to 1 to enable Transfer
+
+  # The following settings are predefined. Change them only if necessary.
+  network_name    = "network"          # Name of the network
+  subnet_name     = "subnet-a"         # Name of the subnet
+  kf_cluster_name = "kafka-cluster"    # Name of the Apache Kafka® cluster
+  kf_username     = "mkf-user"         # Username of the Apache Kafka® cluster
+  kf_topic        = "sensors"          # Name of the Apache Kafka® topic
+  mg_cluster_name = "mongodb-cluster"  # Name of the MongoDB cluster
+  mg_db_name      = "db1"              # Name of the MongoDB cluster database
+  mg_username     = "mmg-user"         # Username of the MongoDB cluster
+  transfer_name   = "mkf-mmg-transfer" # Name of the transfer from the Managed Service for Apache Kafka® to the Managed Service for MongoDB
 }
 
+# Network infrastructure
+
 resource "yandex_vpc_network" "network" {
-  description = "Network for the Managed Service for Apache Kafka® and MongoDB clusters"
-  name        = "network"
+  description = "Network for the Managed Service for Apache Kafka® and Managed Service for MongoDB clusters"
+  name        = local.network_name
 }
 
 resource "yandex_vpc_subnet" "subnet-a" {
   description    = "Subnet in the ru-central1-a availability zone"
-  name           = "subnet-a"
+  name           = local.subnet_name
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["10.1.0.0/16"]
@@ -60,9 +74,11 @@ resource "yandex_vpc_security_group" "clusters-security-group" {
   }
 }
 
+# Infrastructure for the Managed Service for Apache Kafka® cluster
+
 resource "yandex_mdb_kafka_cluster" "kafka-cluster" {
   description        = "Managed Service for Apache Kafka® cluster"
-  name               = "kafka-cluster"
+  name               = local.kf_cluster_name
   environment        = "PRODUCTION"
   network_id         = yandex_vpc_network.network.id
   security_group_ids = [yandex_vpc_security_group.clusters-security-group.id]
@@ -80,51 +96,42 @@ resource "yandex_mdb_kafka_cluster" "kafka-cluster" {
       }
     }
   }
-
-  user {
-    name     = "mkf-user"
-    password = local.source_user_password
-    permission {
-      topic_name = "sensors"
-      role       = "ACCESS_ROLE_CONSUMER"
-    }
-    permission {
-      topic_name = "sensors"
-      role       = "ACCESS_ROLE_PRODUCER"
-    }
-  }
 }
 
-# Managed Service for Apache Kafka® topic.
+# Topic of the Managed Service for Apache Kafka® cluster
 resource "yandex_mdb_kafka_topic" "sensors" {
   cluster_id         = yandex_mdb_kafka_cluster.kafka-cluster.id
-  name               = "sensors"
+  name               = local.kf_topic
   partitions         = 2
   replication_factor = 1
 }
 
+# User of the Managed Service for Apache Kafka® cluster
+resource "yandex_mdb_kafka_user" "mkf-user" {
+  cluster_id = yandex_mdb_kafka_cluster.kafka-cluster.id
+  name       = local.kf_username
+  password   = local.source_user_password
+  permission {
+    topic_name = yandex_mdb_kafka_topic.sensors.name
+    role       = "ACCESS_ROLE_CONSUMER"
+  }
+  permission {
+    topic_name = yandex_mdb_kafka_topic.sensors.name
+    role       = "ACCESS_ROLE_PRODUCER"
+  }
+}
+
+# Infrastructure for the Managed Service for MongoDB cluster
+
 resource "yandex_mdb_mongodb_cluster" "mongodb-cluster" {
   description        = "Managed Service for MongoDB cluster"
-  name               = "mongodb-cluster2"
+  name               = local.mg_cluster_name
   environment        = "PRODUCTION"
   network_id         = yandex_vpc_network.network.id
   security_group_ids = [yandex_vpc_security_group.clusters-security-group.id]
 
   cluster_config {
     version = local.target_mg_version
-  }
-
-  database {
-    name = "db1"
-  }
-
-  user {
-    name     = "mmg-user"
-    password = local.target_user_password
-    permission {
-      database_name = "db1"
-      roles         = ["readWrite"]
-    }
   }
 
   resources_mongod {
@@ -140,11 +147,30 @@ resource "yandex_mdb_mongodb_cluster" "mongodb-cluster" {
   }
 }
 
+# Database of the Managed Service for MongoDB cluster
+resource "yandex_mdb_mongodb_database" "db1" {
+  cluster_id = yandex_mdb_mongodb_cluster.mongodb-cluster.id
+  name       = local.mg_db_name
+}
+
+# User of the Managed Service for MongoDB cluster
+resource "yandex_mdb_mongodb_user" "mmg-user" {
+  cluster_id = yandex_mdb_mongodb_cluster.mongodb-cluster.id
+  name       = local.mg_username
+  password   = local.target_user_password
+  permission {
+    database_name = yandex_mdb_mongodb_database.db1.name
+    roles         = ["readWrite"]
+  }
+}
+
+# Data Transfer infrastructure
+
 resource "yandex_datatransfer_transfer" "mkf-mmg-transfer" {
   count       = local.transfer_enabled
   description = "Transfer from the Managed Service for Apache Kafka® to the Managed Service for MongoDB"
-  name        = "transfer-from-mkf-to-mmg"
+  name        = local.transfer_name
   source_id   = local.source_endpoint_id
   target_id   = local.target_endpoint_id
-  type        = "INCREMENT_ONLY" # Replication data.
+  type        = "INCREMENT_ONLY" # Replication data
 }
